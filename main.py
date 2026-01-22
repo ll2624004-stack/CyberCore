3f069f94e9cd4e2dd240b7d127f85581e9c60794
Loay <ll2624004@gmail.com>
2026-01-22 16:34:30 +0300

    Update main.py
    شباب 🙂 ضفت المكتبات الذي بستخدمها و صح اول سطر مثل حق الباش الذي علمتنا الدكتوره الترم الاول هذا لننا با نشغله في كالي و ضفت اللوان يسمي ANSI Escape Codes عشان الوان في الترمنال

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+# colors OK 
+RED = "\033[91m"
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+CYAN = "\033[96m"
+RESET = "\033[0m"
 
 
-
-
-
-
-
-
+def main():
+    while True:
+        banner()
 
 
 
