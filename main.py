c7bebf507c9acef6946f1680ddd6b4518ca4d00f
Mo-Alsari <mohammedalsari43@gmail.com>
2026-02-03 10:56:27 +0300

    Update main.py
    اصلاح خطاء مسافه

--- main.py
+++ main.py
@@ -161,7 +161,7 @@ def main():
             matches = search_file(raw_input)
             if not matches:
                 print(RED + "File Not Found")
-   input("press enter to try again ..."  + RESET)
+                input("press enter to try again ..."  + RESET)
                 continue
             elif len(matches) == 1:
                 file_path = matches[0]
